a6208524e59396284c5601abf16db949a3025f04
Amy Kapernick <amy@aimhigherweb.design>
2019-10-02 14:48:16 +0800

    added quokka

--- express/server.js
+++ express/server.js
@@ -12,6 +12,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("<h1>Welcome to Quokkas on Demand</h1>");
+  res.write('<img src="/img/quokka.jpg"/>');
   res.end();
 });
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
