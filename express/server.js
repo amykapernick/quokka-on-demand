a8d957e6211b2bbc73b29cae7651aac84259c4f6
Amy Kapernick <amy@aimhigherweb.design>
2019-10-02 17:36:40 +0800

    testing

--- express/server.js
+++ express/server.js
@@ -1,78 +1,41 @@
 "use strict";
-require("dotenv").config();
-
-const express = require("express"),
-  path = require("path"),
-  serverless = require("serverless-http"),
-  app = express(),
-  bodyParser = require("body-parser"),
-  accountSid = process.env.ACCOUNT_SID,
-  authToken = process.env.AUTHTOKEN,
-  client = require("twilio")(accountSid, authToken),
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
+const app = express();
+const bodyParser = require("body-parser"),
   MessagingResponse = require("twilio").twiml.MessagingResponse;
 
 const router = express.Router();
 router.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Welcome to Quokkas on Demand</h1>");
-  res.write('<img src="/img/quokka.jpg"/>');
+  res.write("<h1>Quokka on Demand!</h1>");
   res.end();
 });
-// router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
-// router.post("/", (req, res) => res.json({ postBody: req.body }));
+router.post("/", (req, res) => {
+  const twiml = new MessagingResponse(),
+    recipient = req.body.From,
+    request = req.body.Body,
+    message = twiml.message();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+  console.log(request);
+
+  if (RegExp("quokka", "i").test(request)) {
+    message.body(`This is a quokka`);
+    message.media("https://quokkas.amyskapers.tech/img/quokka.jpg");
+  } else {
+    message.body(`This is not a quokka`);
+    message.media("https://quokkas.amyskapers.tech/img/remi.jpg");
+  }
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+
+  res.end(message.toString());
+});
+
+app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router); // path must route to lambda
-app.use("/", (req, res) => {
-  // client.messages
-  //   .create({
-  //     from: `whatsapp:+14155238886`,
-  //     body: "This is not a quokka",
-  //     to: `whatsapp:+61438984242`
-  //   })
-  //   .then(message => console.log(message.sid));
-});
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.post("/.netlify/functions/server", function(req, res) {
-  const twiml = new MessagingResponse(),
-    recipient = req.body.From,
-    request = req.body.Body,
-    message = twiml.message();
-
-  if (RegExp("quokka", "i").test(request)) {
-    message.body(`This is a quokka`);
-    message.media("https://quokkas.amyskapers.tech/img/quokka.jpg");
-  } else {
-    message.body(`This is not a quokka`);
-    message.media("https://quokkas.amyskapers.tech/img/remi.jpg");
-  }
-
-  res.writeHead(200, { "Content-Type": "text/xml" });
-
-  res.end(message.toString());
-});
-
-app.post("/express/server.js", (req, res) => {
-  console.log("post running");
-  const twiml = new MessagingResponse(),
-    recipient = req.body.From,
-    request = req.body.Body,
-    message = twiml.message();
-
-  if (RegExp("quokka", "i").test(request)) {
-    message.body(`This is a quokka`);
-    message.media("https://quokkas.amyskapers.tech/img/quokka.jpg");
-  } else {
-    message.body(`This is not a quokka`);
-    message.media("https://quokkas.amyskapers.tech/img/remi.jpg");
-  }
-
-  res.writeHead(200, { "Content-Type": "text/xml" });
-
-  res.end(message.toString());
-});
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
