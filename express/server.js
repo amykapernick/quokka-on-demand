2400f42152549d7d6f2b14046324f5184041c104
Amy Kapernick <amy@aimhigherweb.design>
2019-10-02 14:46:14 +0800

    added express and env variables

--- express/server.js
+++ express/server.js
@@ -1,22 +1,46 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+"use strict";
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser"),
+  accountSid = process.env.ACCOUNT_SID,
+  authToken = process.env.AUTHTOKEN,
+  MessagingResponse = require("twilio").twiml.MessagingResponse;
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/", (req, res) => {
+  const twiml = new MessagingResponse(),
+    recipient = req.body.From,
+    request = req.body.Body,
+    message = twiml.message();
+
+  if (RegExp("quokka", "i").test(request)) {
+    message.body(`This is a quokka`);
+    message.media("https://quokkas.amyskapers.tech/img/quokka.jpg");
+  } else {
+    message.body(`This is not a quokka`);
+    message.media("https://quokkas.amyskapers.tech/img/remi.jpg");
+  }
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+
+  res.end(message.toString());
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
