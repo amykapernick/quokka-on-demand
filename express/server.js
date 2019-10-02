f96b97d8b96d98e6dcdeb7a90dce68fb6d48717d
Amy Kapernick <amy@aimhigherweb.design>
2019-10-02 16:38:06 +0800

    testing

--- express/server.js
+++ express/server.js
@@ -21,24 +21,37 @@ router.get("/", (req, res) => {
 // router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
 // router.post("/", (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/.netlify/functions/server", router); // path must route to lambda
 app.use("/", (req, res) => {
-  client.messages
-    .create({
-      from: `whatsapp:+14155238886`,
-      body: "This is not a quokka",
-      to: `whatsapp:+61438984242`
-    })
-    .then(message => console.log(message.sid));
+  // client.messages
+  //   .create({
+  //     from: `whatsapp:+14155238886`,
+  //     body: "This is not a quokka",
+  //     to: `whatsapp:+61438984242`
+  //   })
+  //   .then(message => console.log(message.sid));
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-console.log("running");
-
 router.get("/.netlify/functions/server", function(req, res) {
-  console.log("get running");
+  const twiml = new MessagingResponse(),
+    recipient = req.body.From,
+    request = req.body.Body,
+    message = twiml.message();
+
+  if (RegExp("quokka", "i").test(request)) {
+    message.body(`This is a quokka`);
+    message.media("https://quokkas.amyskapers.tech/img/quokka.jpg");
+  } else {
+    message.body(`This is not a quokka`);
+    message.media("https://quokkas.amyskapers.tech/img/remi.jpg");
+  }
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+
+  res.end(message.toString());
 });
 
 app.post("/express/server.js", (req, res) => {
