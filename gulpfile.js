a4eaf29d726004fdd846b9cd847906fcb49aa0b8
Amy Kapernick <amy@aimhigherweb.design>
2019-10-07 21:16:24 +0800

    fiixing scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,32 @@
 //Variables
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var sourcemaps = require("gulp-sourcemaps");
-var browserSync = require("browser-sync").create();
-var reload = browserSync.reload;
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create()
+var reload = browserSync.reload
 
 //File Paths
-var sassFiles = "scss/**/*.scss",
-  mainSassFile = "scss/style.scss",
-  cssFiles = "./public/",
-  sourceMaps = "scss/maps";
+var sassFiles = 'scss/**/*.scss',
+	mainSassFile = 'scss/style.scss',
+	cssFiles = './public/'
 
 //Compile main sass into css
 
 function sassy() {
-  return gulp
-    .src(mainSassFile)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on("error", sass.logError)) //Using gulp-sass
-    .pipe(sourcemaps.write(sourceMaps))
-    .pipe(gulp.dest(cssFiles));
+	return gulp
+		.src(mainSassFile)
+		.pipe(sass().on('error', sass.logError)) //Using gulp-sass
+		.pipe(gulp.dest(cssFiles))
 }
 
 //Watch for changes in sass files and running sass compile
 function watch() {
-  browserSync.init({
-    proxy: "http://localhost:3000/"
-  });
+	browserSync.init({
+		proxy: 'http://localhost:3000/',
+	})
 
-  gulp.watch(sassFiles, sassy);
-  gulp.watch(["./scss/**/*.scss", "./index.html"]).on("change", reload);
+	gulp.watch(sassFiles, sassy)
+	gulp.watch(['./scss/**/*.scss', './index.html']).on('change', reload)
 }
 
-exports.sassy = sassy;
-exports.watch = watch;
+exports.sassy = sassy
+exports.watch = watch
