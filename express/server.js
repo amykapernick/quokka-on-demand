51e15396738cbbeb2249675b601a79281352e471
Amy Kapernick <amy@aimhigherweb.design>
2019-10-08 07:40:49 +0800

    roll back server

--- express/server.js
+++ express/server.js
@@ -3,40 +3,16 @@ const express = require('express')
 const path = require('path')
 const serverless = require('serverless-http')
 const app = express()
-const bodyParser = require('body-parser'),
-	MessagingResponse = require('twilio').twiml.MessagingResponse
+const bodyParser = require('body-parser')
 
 const router = express.Router()
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
-router.all('/', (req, res) => {
+router.get('/', (req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/html' })
-	res.write('<h1>Quokkas on Demand!</h1>')
-	res.write('<img src="/img/quokka.jpg" />')
-	res.sendFile('/index.html')
+	res.write('<h1>Hello from Express.js!</h1>')
 	res.end()
 })
-router.post('/', (req, res) => {
-	const twiml = new MessagingResponse(),
-		recipient = req.body.From,
-		request = req.body.Body,
-		message = twiml.message()
-
-	console.log(request)
-
-	if (RegExp('quokka', 'i').test(request)) {
-		message.body(`This is a quokka`)
-		message.media('https://quokkas.amyskapers.tech/img/quokka.jpg')
-	} else {
-		message.body(`This is not a quokka`)
-		message.media('https://quokkas.amyskapers.tech/img/remi.jpg')
-	}
-
-	res.writeHead(200, { 'Content-Type': 'text/xml' })
-
-	res.end(message.toString())
-})
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }))
+router.post('/', (req, res) => res.json({ postBody: req.body }))
 
 app.use(bodyParser.json())
 app.use('/.netlify/functions/server', router) // path must route to lambda
