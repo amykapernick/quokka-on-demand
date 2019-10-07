461905464770082a9fab8d3669fb347968ee37b7
Amy Kapernick <amy@aimhigherweb.design>
2019-10-07 21:35:53 +0800

    change to all

--- express/server.js
+++ express/server.js
@@ -10,7 +10,7 @@ const router = express.Router()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-router.get('/', (req, res) => {
+router.all('/', (req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/html' })
 	// res.write("<h1>Quokkas on Demand!</h1>");
 	// res.write('<img src="/img/quokka.jpg" />');
