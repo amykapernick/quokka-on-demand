3172c511e29122ee68446c2c6331230e4a48324b
Amy Kapernick <amy@aimhigherweb.design>
2019-10-08 09:24:20 +0800

    added ability to request image for tesitng

--- local.js
+++ local.js
@@ -18,9 +18,13 @@ app.post('/', (req, res) => {
 	const twiml = new MessagingResponse(),
 		recipient = req.body.From,
 		request = req.body.Body,
-		message = twiml.message(),
-		photo = Math.floor(Math.random() * 12)
-	let type = 'jpg'
+		message = twiml.message()
+	let photo = Math.floor(Math.random() * 12),
+		type = 'jpg'
+
+	if (RegExp(/\d+/).test(request) && request.match(/(\d+)/)[0] < 12) {
+		photo = request.match(/(\d+)/)[0]
+	}
 
 	if (RegExp('quokka', 'i').test(request)) {
 		message.body(`This is a quokka`)
@@ -28,9 +32,11 @@ app.post('/', (req, res) => {
 	} else {
 		if (photo > 9) {
 			type = 'gif'
+			message.body(`This is not a quokka https://quokkas.amyskapers.tech/img/not_quokka_(${photo}).${type}`)
+		} else {
+			message.body(`This is not a quokka`)
+			message.media(`https://quokkas.amyskapers.tech/img/not_quokka_(${photo}).${type}`)
 		}
-		message.body(`This is not a quokka`)
-		message.media(`https://quokkas.amyskapers.tech/img/not_quokka_(${photo}).${type}`)
 	}
 
 	res.writeHead(200, { 'Content-Type': 'text/xml' })
